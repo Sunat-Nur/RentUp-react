a9dff74190fd8d0f26c342396ec67fe92c0e3126
Sunat_nur <uzbekistan9699@gmail.com>
2024-01-18 12:26:06 +0900

    fix: customized integration of MUI

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,42 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+import {createRoot} from 'react-dom/client';  // yarn start berganimizda eng muhim narsa react-dom,
+// bu  real-domni virtual-dom ga ulab beradigan package. hozir react-dom ni ichidan create dagan toolni olyabmiz
+import {Provider} from 'react-redux';  //  Provider react-reduxni componenti, bizni componentlarni redux ga bog'lovchi component // ma'lumotlar bunkeri
+import {store} from './app/store';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
 import './css/index.css';
 
-const container = document.getElementById('root')!;
-const root = createRoot(container);
+import CssBaseline from '@mui/material/CssBaseline';
+import {ThemeProvider} from '@mui/material/styles'; // themeProvider materila uni reactimizga integratsiya qilyabmiz
+import theme from './app/MaterialTheme';  //mui css fream-work
+import ReactDOM from "react-dom";
 
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+// const container = document.getElementById('root')!;
+// document bu yerda real-dom hisoblanadi,
+
+// real-domdan getElementById bilan o'zagina qo'lga olib, container nomi bilan saqlayabmiz
+// const root = createRoot(container);
+// bu yerda containerni react-dom package ni ichida paste qilyabmiz
+// natijada yangi root berilyabdi. yangi root da ham real-dom ham virtual dom integratsiya qilinga
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline/>
+                    <App/>
+            </ThemeProvider>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
+
+//  Provider react-reduxni componenti, bizni componentlarni redux ga bog'lovchi component  // store reduxni storage hisoblanadi
+
+
+// React ni tez ishlash sababi
+// 1- virtual dom 2- barcha ma'lumotlar json formatda boradi
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
